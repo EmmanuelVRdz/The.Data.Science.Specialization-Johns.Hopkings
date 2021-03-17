6d815697f7ca21a406688470cfab8dca20539c41
Emmanuel Valdés Rodríguez <emmvrdz@gmail.com>
2021-03-16 19:16:30 -0600

    second course of the data science specialization

---
+++ 2 R Programming.R
@@ -0,0 +1,24 @@
+######## R Programming ########
+
+
+
+#### Working Directory ####
+
+# Before starting using R, one key concept is "Working Directory". This is the
+# directory/folder on your computer where you will store project files, data, and 
+# code. It's important that we tell R where the working directory is that you
+# will be using so that it knows where to find the appropiate file ( the working
+# directory can be any directory on your computer).
+
+# How to figure out what your working directory is? Use the Get Working Directory
+# Function <getwd()>.
+
+getwd()
+
+# All the files, code, or data that you may use in an R project should be stored 
+# in the Working Directory. You can change your working directory with the 
+# Set Working Directory Function <setwd()>
+
+setwd()
+
+
